6a8c954989484eaa83d8946b86dfcebcc4a50324
koteyka5000 <85498873+koteyka5000@users.noreply.github.com>
2021-06-07 17:17:00 +0300

    Test, full rewrite
    This is a test file :)

--- foo.py
+++ foo.py
@@ -1,2 +1,15 @@
-print('hi')
-print('lol')
+class Test:
+    def __init__(self, color, type, id, lol):
+        self.color = color
+        self.type = type
+        self.id = id
+        self.lol = lol
+    def info():
+   		return {'color': self.color, 'type': self.type, 'id': self.id, 'lol': self.lol}
+	
+	def check():
+		if self.color == 'red' and self.lol == 'hehe' or self.lol == 'haha':
+			return True
+		else:
+			return False
+    
