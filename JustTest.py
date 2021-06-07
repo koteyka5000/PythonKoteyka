71ea3a0f93bc87a28360a5692827acc6e6ada836
koteyka5000 <85498873+koteyka5000@users.noreply.github.com>
2021-06-07 17:37:39 +0300

    New programm :)
    Lol

--- JustTest.py
+++ JustTest.py
@@ -1,14 +1,16 @@
 class Test:
-    def __init__(self, color, type, id, lol):
-        self.color = color
-        self.type = type
-        self.id = id
-        self.lol = lol
-    def info():
-   	    return {'color': self.color, 'type': self.type, 'id': self.id, 'lol': self.lol}
-	
-	def check():
-		if self.color == 'red' and self.lol == 'hehe' or self.lol == 'haha':
-			return True
-		else:
-			return False
+	def __init__(self, i, q):
+		self.i = i
+		self.q = q
+	def info(self):
+		return {'i': self.i,  'q': self.q}
+	def lol(self):
+		g = (1, 2, 3, 4, 5, 6, 7, 8)
+		l = []
+		for gg in g:
+			if gg % 2 == 0:
+				l.append(gg)
+			else:
+				l.append(g[gg]-1)
+		return l
+			
